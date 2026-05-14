ecece4c2fd7a6e8240c13f3a5546c1e05dfedcfa
elnovic <elnovicmalonda2@gmail.com>
2026-05-14 03:58:25 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
+
+app = FastAPI()
+
+# Montage des fichiers statiques et templates
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def get_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
